e0501d5d1be229af7221518e677d43c6ed6280e7
José Ángel Gallardo Tabares <angelgallardo@tec.mx>
2022-01-18 23:04:34 -0600

    added wordclouds

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,13 @@ import { useState } from 'react'
 
 export default function Home() {
   const [isCompleted,setIsCompleted,] = useState(false)
-  const result = null;
+  const [data,setData] = useState({keyword:"",
+                                    email:"",
+                                    positive:0,
+                                    negative:0,
+                                    neutral:0,
+                                    wordcloud_neg:{},
+                                    wordcloud_pos:{}})
   const getSentiment = async event => {
     event.preventDefault()
     const options ={
@@ -24,33 +30,43 @@ export default function Home() {
           'Content-Type': 'application/json'
       }
     }
+    setIsCompleted(false)
     const res = await fetch('http://localhost:8000/api/sentiment/',options)
-    result = await res.json()
+    const result = await res.json()
+    setData(result)
     console.log('result',result)
     setIsCompleted(true)
+    return result;
   }
 
   if(process.browser){
     return(
       <div>
-        {!isCompleted?
+        <div>
+          <form onSubmit={getSentiment}>
+          <label htmlFor="name">Keyword</label>
+          <input id="name" name="name" type="text" autoComplete="name" required />
+          <button type="submit">Submit</button>
+          </form> 
+        </div>
+        {isCompleted?
           <div>
-            <form onSubmit={getSentiment}>
-            <label htmlFor="name">Keyword</label>
-            <input id="name" name="name" type="text" autoComplete="name" required />
-            <button type="submit">Submit</button>
-            </form> 
-          </div>:<div>
             <div>
-              <h1>{result.positive}</h1>
-              {/* <ReactWordcloud words={result.wordcloud_pos} /> */}
+              <h1>positive</h1>
+              <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [20,100],deterministic:true,enableOptimizations:true,}} 
+                words={data.wordcloud_pos} 
+                size = {[600, 600]}
+                />
             </div>
     
             <div>
-              <h1>{result.negative}</h1>
-              {/* <ReactWordcloud words={result.wordcloud_neg} /> */}
+              <h1>negative</h1>
+              <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [20,100],deterministic:true,enableOptimizations:true,}}
+              words={data.wordcloud_neg}
+              size = {[600, 600]} 
+              />
             </div>          
-          </div>
+          </div>:<div></div>
         }
       </div>
       //
