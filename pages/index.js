cf56c3071797cf13f72170a5c75ee493975f467b
Jose Angel Gallardo Tabares <JGALLARDO@kmx.keyence.com>
2022-01-19 17:58:53 -0600

    Styled website rough draft

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import ReactWordcloud from 'react-wordcloud';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale.css';
 import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import { Bars } from 'react-loader-spinner'
 
 export default function Home() {
   const [isCompleted,setIsCompleted,] = useState(false)
+  const [isLoading,setIsLoading,] = useState(false)
   const [data,setData] = useState({keyword:"",
                                     email:"",
                                     positive:0,
@@ -31,44 +34,71 @@ export default function Home() {
       }
     }
     setIsCompleted(false)
+    setIsLoading(true)
     const res = await fetch('http://localhost:8000/api/sentiment/',options)
     const result = await res.json()
     setData(result)
     console.log('result',result)
+    setIsLoading(false)
     setIsCompleted(true)
     return result;
   }
 
   if(process.browser){
     return(
-      <div>
-        <div>
-          <form onSubmit={getSentiment}>
-          <label htmlFor="name">Keyword</label>
-          <input id="name" name="name" type="text" autoComplete="name" required />
-          <button type="submit">Submit</button>
-          </form> 
-        </div>
-        {isCompleted?
+      <main className={styles.Main}>
+        <div className={styles.InnerContent}>
+          <div className={styles.Form}>
+            <form onSubmit={getSentiment}>
+            <h1 className={styles.keywordText}>Analyze sentiment of any topic!</h1>
+            <input id="name" name="name" type="text" autoComplete="name" required className={styles.keywordBox}/>
+            <button type="submit" className={styles.keywordSubmit}>Search</button>
+            </form> 
+          </div>
+          {isLoading?
+            <div>
+              <div className={styles.loadingBar} >
+                <Bars color="#408080" height={80} width={120} />
+              </div>
+              <p className={styles.loadingText}>PROCESSING INFO</p>
+            </div>:<div></div>
+          }
+          {isCompleted?
           <div>
             <div>
-              <h1>positive</h1>
-              <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [20,100],deterministic:true,enableOptimizations:true,}} 
-                words={data.wordcloud_pos} 
-                size = {[600, 600]}
-                />
+              <div className={styles.gradientParent}> 
+                <p className={styles.gradientNeg}>{data.negative}% </p>
+                <p className={styles.gradientNeu}>{data.neutral}% </p>
+                <p className={styles.gradientPos}>{data.positive}% </p>
+              </div>
             </div>
-    
-            <div>
-              <h1>negative</h1>
-              <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [20,100],deterministic:true,enableOptimizations:true,}}
-              words={data.wordcloud_neg}
-              size = {[600, 600]} 
-              />
-            </div>          
-          </div>:<div></div>
-        }
-      </div>
+            <div className={styles.wordcloudGrid}>
+              <div className={styles.leftGrid}>
+                <h1 className={styles.wordcloudText}>Recurring positive words</h1>
+                <div className={styles.wordcloudBox}>
+                  <div className={styles.wordCloud}>
+                    <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [30,100],deterministic:true,enableOptimizations:true,colors:["#1F994E","#12592D","#2BD96E","#2EE674","#26C261"],}} 
+                    words={data.wordcloud_pos} 
+                    />
+                  </div>
+                </div>
+              </div>
+      
+              <div className={styles.rightGrid}>
+                <h1 className={styles.wordcloudText}>Recurring negative words</h1>
+                <div className={styles.wordcloudBox}>
+                  <div className={styles.wordCloud}>
+                    <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [30,100],deterministic:true,enableOptimizations:true,colors:["#992822","#591714","#D93830","#E63B32","#C7332C"],}}
+                    words={data.wordcloud_neg}
+                    />
+                  </div>
+                </div>
+              </div>     
+            </div>     
+            </div>:<div></div>
+          }
+        </div>
+      </main>
       //
       )
   }
