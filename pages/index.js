6b8a355a5d69684a0a74eb53bdc13dd4ebcc5cb0
Jose Angel Gallardo Tabares <JGALLARDO@kmx.keyence.com>
2022-01-26 13:51:27 -0600

    implemented frontpage

--- pages/index.js
+++ pages/index.js
@@ -1,194 +1,80 @@
-import ReactWordcloud from 'react-wordcloud';
-import 'tippy.js/dist/tippy.css';
-import 'tippy.js/animations/scale.css';
-import { useState } from 'react'
 import styles from '../styles/Home.module.css'
-import { Bars } from 'react-loader-spinner'
-import {Doughnut} from 'react-chartjs-2';
-import 'chart.js/auto';
-import { Resizable } from "re-resizable";
+import Link from 'next/link'
+import Head from 'next/head'
+import {SiNextdotjs} from 'react-icons/si'
+import {SiReact} from 'react-icons/si'
+import {SiDjango} from 'react-icons/si'
 
-export default function Home({trendData,trendError}) {
-  const [isCompleted,setIsCompleted,] = useState(false)
-  const [isLoading,setIsLoading,] = useState(false)
-  const [keywordSearch,setKeyword] = useState("")
-  var pos_parse = 0.0;
-  var neu_parse = 0.0;
-  var neg_parse = 0.0;
-  const [data,setData] = useState({keyword:"",
-                                    email:"",
-                                    positive:0,
-                                    negative:0,
-                                    neutral:0,
-                                    wordcloud_neg:{},
-                                    wordcloud_pos:{}})
 
-  var dataChart = null; 
-
-  const getSentiment = async event => {
-    event.preventDefault()
-    const options ={
-      method: "POST",
-      body: JSON.stringify(
-        {
-          keyword: keywordSearch,
-          email: "",
-          positive: 0,
-          negative: 0,
-          neutral: 0,
-          wordcloud_neg: {},
-          wordcloud_pos: {}
-        }),
-      headers:{
-          'Content-Type': 'application/json'
-      }
-    }
-    setIsCompleted(false)
-    setIsLoading(true)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/sentiment/`,options)
-    const result = await res.json()
-    setData(result)
-    setIsLoading(false)
-    setIsCompleted(true)
-    return result;
-  }
-  if(isCompleted){
-    pos_parse = parseFloat(data.positive).toFixed(2)
-    neu_parse = parseFloat(data.neutral).toFixed(2)
-    neg_parse = parseFloat(data.negative).toFixed(2)
-
-    dataChart = {
-      labels: [
-        'Positive',
-        'Negative',
-        'Neutral'
-      ],
-      datasets: [{
-        data: [data.positive, data.negative, data.neutral],
-        backgroundColor: [
-          '#43A047',
-          '#E53935',
-          '#00ACC1'
-        ],
-        hoverBackgroundColor: [
-          '#1B5E20',
-          '#B71C1C',
-          '#006064'
-        ]
-      }],
-      
-    };
-
-  }
-
-  if(process.browser){
-    return(
-      <main>
-        <div className={styles.Main}>
-          <div className={styles.InnerContent}>
-            <div className={styles.Form}>
-              <form onSubmit={getSentiment}>
-              <h1 className={styles.keywordText}>Analyze sentiment of any topic!</h1>
-              <input id="name" name="name" type="text" autoComplete="on" required className={styles.keywordBox} onChange={(e)=>{setKeyword(e.target.value);}} placeholder={keywordSearch}/>
-              <button type="submit" className={styles.keywordSubmit}>Search</button>
-              </form> 
+export default function Home() {
+  return(
+    <div>
+      <Head>
+        <title>
+          Ángel Gallardo | Home
+        </title>
+        <meta name="description" content="Angel Gallardo's portfolio"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <main className={styles.Main}>
+        <div className={styles.Wrapper}>
+          <div className={styles.Content}>
+            <div className={styles.NavBar}>
+              <div className={styles.NavContent}>
+                <Link href={"/"}>HOME</Link>
+              </div>
+              <div className={styles.NavContent}>
+                <Link href={"/sentiment/"}>SENTIMENT</Link>
+              </div>
+              <div className={styles.NavContent}>
+                <Link href={"/"}>PORTFOLIO</Link>
+                </div>            
             </div>
-            <div className={styles.Form2}>
-              <form onSubmit={getSentiment}>
-                <button type="submit" value={trendData.trend_1} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_1}</button>
-                <button type="submit" value={trendData.trend_2} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_2}</button>
-                <button type="submit" value={trendData.trend_3} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_3}</button>
-                <button type="submit" value={trendData.trend_4} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_4}</button>
-                <button type="submit" value={trendData.trend_5} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_5}</button>
-              </form>
+            <body className={styles.Body}>
+              <h1 className={styles.Intro}>
+                HEY, I'M ÁNGEL GALLARDO
+              </h1>
+              <p className={styles.Headline}>
+                I'M A POST-GRADUATE PROFESSIONAL WITH A PASSION FOR PROGRAMMING, MACHINE LEARNING, AND MECHATRONICS. I CURRENTLY WORK AT KEYENCE AS A TECH SUPPORT ENGINEER. I'M ALSO AN ASPIRING PHOTOGRAPHER.
+              </p>
+              <h2 className={styles.MySkills}>
+                MY SKILLS
+              </h2>
+              <div className={styles.SkillsColumn}>
+                <p className={styles.Skillset}>
+                  Front End
+                </p>
+                <p className={styles.Skillset}>
+                  Back End
+                </p>
+                <p className={styles.Skillset}>
+                  Tools
+                </p>
+              </div>
+            </body>
+            <div className={styles.SkillsIcons}>
+              <div className={styles.Icon}>
+                <div className={styles.IconSeparation}>
+                  <SiNextdotjs size={70} color="gold" />
+                </div>
+                <div className={styles.IconSeparation}>
+                  <SiReact size={70} color="gold" />
+                </div>
+              </div>
+              <div className={styles.Icon}>
+                <SiDjango size={70} color="gold" />
+              </div>
             </div>
-            {isLoading?
-              <div>
-                <div className={styles.loadingBar} >
-                  <Bars color="#212121" height={80} width={120} />
-                </div>
-                <p className={styles.loadingText}>PROCESSING INFO</p>
-              </div>:<div></div>
-            }
-            {isCompleted?
-            <div>
-              <div className={styles.wordcloudGrid}>
-                <div className={styles.leftGrid}>
-                  <div className={styles.reportText}> 
-                    <p>{data.keyword} returns a {pos_parse}% of positive opinions and {neg_parse}% of negative opinions </p>
-                  </div>
-                  <div className={styles.dataChart}>
-                    <Doughnut data={dataChart} />
-                  </div>
-                </div>
-        
-                <div className={styles.rightGrid}>
-                  <h1 className={styles.wordcloudText}>Recurring positive words</h1>
-                  <div className={styles.wordcloudBox}>
-                    <div className={styles.wordCloud}>
-                      <Resizable>
-                        <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [15,60],colors:["#1B5E20","#2E7D32","#388E3C","#43A047"],}} 
-                        words={data.wordcloud_pos} 
-                        />
-                      </Resizable>
-                    </div>
-                  </div>
-                  <h1 className={styles.wordcloudText}>Recurring negative words</h1>
-                  <div className={styles.wordcloudBox}>
-                    <div className={styles.wordCloud}>
-                      <Resizable>
-                        <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [15,60],colors:["#B71C1C","#C62828","#D32F2F","#E53935"],}}
-                        words={data.wordcloud_neg}
-                        />
-                      </Resizable>
-                    </div>
-                  </div>
-                </div>     
-              </div>     
-              </div>:<div></div>
-            }
           </div>
         </div>
-      </main>
-      //
-      )
-  }
-  else{
-    return <div></div>
-  }
-  
+      </main>      
+    </div>
+  )
 }
 
-
-export async function getServerSideProps(){
-  let trendData = []
-  let trendError = null;
-  try {
-    const options ={
-      method: "POST",
-      body: JSON.stringify(
-        {
-          "trend_1": "",
-          "trend_2": "",
-          "trend_3": "",
-          "trend_4": "",
-          "trend_5": ""
-        }),
-      headers:{
-          'Content-Type': 'application/json'
-      }
-    }
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/trends/`,options)
-    trendData = await response.json()
-  } catch (err) {
-    console.log('err', err);
-    trendError = err.message?err.message:"Something went wrong"
-  }
-
-  return{
-    props:{
-      trendData,
-      trendError,
-    }
-  }
-}
---
+++ pages/sentiment.js
@@ -0,0 +1,194 @@
+import ReactWordcloud from 'react-wordcloud';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css';
+import { useState } from 'react'
+import styles from '../styles/Sentiment.module.css'
+import { Bars } from 'react-loader-spinner'
+import {Doughnut} from 'react-chartjs-2';
+import 'chart.js/auto';
+import { Resizable } from "re-resizable";
+
+export default function Home({trendData,trendError}) {
+  const [isCompleted,setIsCompleted,] = useState(false)
+  const [isLoading,setIsLoading,] = useState(false)
+  const [keywordSearch,setKeyword] = useState("")
+  var pos_parse = 0.0;
+  var neu_parse = 0.0;
+  var neg_parse = 0.0;
+  const [data,setData] = useState({keyword:"",
+                                    email:"",
+                                    positive:0,
+                                    negative:0,
+                                    neutral:0,
+                                    wordcloud_neg:{},
+                                    wordcloud_pos:{}})
+
+  var dataChart = null; 
+
+  const getSentiment = async event => {
+    event.preventDefault()
+    const options ={
+      method: "POST",
+      body: JSON.stringify(
+        {
+          keyword: keywordSearch,
+          email: "",
+          positive: 0,
+          negative: 0,
+          neutral: 0,
+          wordcloud_neg: {},
+          wordcloud_pos: {}
+        }),
+      headers:{
+          'Content-Type': 'application/json'
+      }
+    }
+    setIsCompleted(false)
+    setIsLoading(true)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/sentiment/`,options)
+    const result = await res.json()
+    setData(result)
+    setIsLoading(false)
+    setIsCompleted(true)
+    return result;
+  }
+  if(isCompleted){
+    pos_parse = parseFloat(data.positive).toFixed(2)
+    neu_parse = parseFloat(data.neutral).toFixed(2)
+    neg_parse = parseFloat(data.negative).toFixed(2)
+
+    dataChart = {
+      labels: [
+        'Positive',
+        'Negative',
+        'Neutral'
+      ],
+      datasets: [{
+        data: [data.positive, data.negative, data.neutral],
+        backgroundColor: [
+          '#43A047',
+          '#E53935',
+          '#00ACC1'
+        ],
+        hoverBackgroundColor: [
+          '#1B5E20',
+          '#B71C1C',
+          '#006064'
+        ]
+      }],
+      
+    };
+
+  }
+
+  if(process.browser){
+    return(
+      <main>
+        <div className={styles.Main}>
+          <div className={styles.InnerContent}>
+            <div className={styles.Form}>
+              <form onSubmit={getSentiment}>
+              <h1 className={styles.keywordText}>Analyze sentiment of any topic!</h1>
+              <input id="name" name="name" type="text" autoComplete="on" required className={styles.keywordBox} onChange={(e)=>{setKeyword(e.target.value);}} placeholder={keywordSearch}/>
+              <button type="submit" className={styles.keywordSubmit}>Search</button>
+              </form> 
+            </div>
+            <div className={styles.Form2}>
+              <form onSubmit={getSentiment}>
+                <button type="submit" value={trendData.trend_1} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_1}</button>
+                <button type="submit" value={trendData.trend_2} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_2}</button>
+                <button type="submit" value={trendData.trend_3} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_3}</button>
+                <button type="submit" value={trendData.trend_4} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_4}</button>
+                <button type="submit" value={trendData.trend_5} onClick={(e)=>{setKeyword(e.target.value);}} className={styles.buttonTrends}>{trendData.trend_5}</button>
+              </form>
+            </div>
+            {isLoading?
+              <div>
+                <div className={styles.loadingBar} >
+                  <Bars color="#212121" height={80} width={120} />
+                </div>
+                <p className={styles.loadingText}>PROCESSING INFO</p>
+              </div>:<div></div>
+            }
+            {isCompleted?
+            <div>
+              <div className={styles.wordcloudGrid}>
+                <div className={styles.leftGrid}>
+                  <div className={styles.reportText}> 
+                    <p>{data.keyword} returns a {pos_parse}% of positive opinions and {neg_parse}% of negative opinions </p>
+                  </div>
+                  <div className={styles.dataChart}>
+                    <Doughnut data={dataChart} />
+                  </div>
+                </div>
+        
+                <div className={styles.rightGrid}>
+                  <h1 className={styles.wordcloudText}>Recurring positive words</h1>
+                  <div className={styles.wordcloudBox}>
+                    <div className={styles.wordCloud}>
+                      <Resizable>
+                        <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [15,60],colors:["#1B5E20","#2E7D32","#388E3C","#43A047"],}} 
+                        words={data.wordcloud_pos} 
+                        />
+                      </Resizable>
+                    </div>
+                  </div>
+                  <h1 className={styles.wordcloudText}>Recurring negative words</h1>
+                  <div className={styles.wordcloudBox}>
+                    <div className={styles.wordCloud}>
+                      <Resizable>
+                        <ReactWordcloud options={{rotations: 2,rotationAngles: [-90, 0],fontSizes: [15,60],colors:["#B71C1C","#C62828","#D32F2F","#E53935"],}}
+                        words={data.wordcloud_neg}
+                        />
+                      </Resizable>
+                    </div>
+                  </div>
+                </div>     
+              </div>     
+              </div>:<div></div>
+            }
+          </div>
+        </div>
+      </main>
+      //
+      )
+  }
+  else{
+    return <div></div>
+  }
+  
+}
+
+
+export async function getServerSideProps(){
+  let trendData = []
+  let trendError = null;
+  try {
+    const options ={
+      method: "POST",
+      body: JSON.stringify(
+        {
+          "trend_1": "",
+          "trend_2": "",
+          "trend_3": "",
+          "trend_4": "",
+          "trend_5": ""
+        }),
+      headers:{
+          'Content-Type': 'application/json'
+      }
+    }
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/trends/`,options)
+    trendData = await response.json()
+  } catch (err) {
+    console.log('err', err);
+    trendError = err.message?err.message:"Something went wrong"
+  }
+
+  return{
+    props:{
+      trendData,
+      trendError,
+    }
+  }
+}
